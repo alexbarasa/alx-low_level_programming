539645e93a5d5e5a994787b548ea0abb5794ff80
Alex Barasa <alexbarasa847@gmail.com>
2023-12-14 23:35:44 +0300

    C programming

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <float.h>
+/**
+ * main - Entry point
+ * @argc: Argument count
+ * @argv: Argument vector
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char **argv)
+{
+	printf("size of char %lu byte(s)\n", sizeof(char));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %ld byte(s)\n", sizeof(float));
+	return (0);
+}
