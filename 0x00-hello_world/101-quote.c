cf5d34e79a8fcf365805928d7e36d739e61aecfa
Alex Barasa <alexbarasa847@gmail.com>
2023-12-15 00:41:17 +0300

    C programming

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: 1 Not Successful
+ */
+int main(void)
+{
+	const char *message = "and that piece of art is useful\" - "
+	"Dora Korpar, 2015-10-19\n";
+
+	write(2, message, strlen(message));
+	return (0);
+}
