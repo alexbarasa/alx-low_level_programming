3ca6fb6a0d88febcc4299c7e385a1819655448c3
Alex Barasa <alexbarasa847@gmail.com>
2023-12-14 20:50:50 +0300

    C programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
-
+/*This program illustrates how to use puts function*/
 int main(void)
-{
+/*This main function take no arguements and returns int*/
+{	
 	puts("\"Programming is like building a multilingual puzzle\n");
-        return (0);	
+	return (0);	
 }
