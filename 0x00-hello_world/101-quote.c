b36c02663c2baddbd829c22184b9586e4a682b4a
Alex Barasa <alexbarasa847@gmail.com>
2023-12-15 11:16:25 +0300

    C programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-	const char s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	const char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(1, "...messag...", 59);
 	return (1);
