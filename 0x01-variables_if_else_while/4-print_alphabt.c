ce6fe69a1efad4f5e6340daa5aa659da4d400456
Alex Barasa <alexbarasa847@gmail.com>
2023-12-15 19:00:09 +0300

    C variables and control flow

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i = 'a';
+	char j;
+
+	for (j = 'z'; i <= j; i++)
+	{
+		if (i != 'e' && i != 'q')
+			putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
