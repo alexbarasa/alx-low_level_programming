b4991a7998d2dcc3a814cd59bfb42c508d94fcc4
alexbarasa <alexbarasa847@gmail.com>
2023-12-19 14:19:12 +0300

    Functions and nested loops

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
 #include <stdio.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
 void print_alphabet(void);
-int main(void)
-{
-	return (0);
-}
+#endif
