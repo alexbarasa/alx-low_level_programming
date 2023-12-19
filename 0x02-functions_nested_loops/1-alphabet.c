26a680f74a9df8df0f73020039d9e1211f0ddaac
alexbarasa <alexbarasa847@gmail.com>
2023-12-19 13:46:25 +0300

    Functions and nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - prints alphabets a-z
+ *
+ * Return: Always 0.
+ */
+int print_alphabet(void)
+{
+	int i;
+	int j = 'z';
+
+	for (i = 'a'; i <= j; i++)
+	{
+		putchar('0' + i);
+	}
+	putchar('\n');
+	return (0);
+}
