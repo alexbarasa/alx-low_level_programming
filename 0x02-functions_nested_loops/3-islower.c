8f80df33d7134079660792b5ca7a89b2ff45c529
alexbarasa <alexbarasa847@gmail.com>
2023-12-19 17:08:54 +0300

    Functions and nested loops

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+/**
+ * _islower - Checks for lower characters
+ *
+ * Return: Always 0.
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
