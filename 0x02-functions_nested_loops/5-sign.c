b7360cfbf1e2e001e26c8e4511fee96f6cc0c2ad
alexbarasa <alexbarasa847@gmail.com>
2023-12-19 20:06:54 +0300

    Functions and nested loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,13 +12,11 @@ int print_sign(int n)
 	int b;
 
 	if (n > 0)
-		c = '+' + p;
-		return (c);
+		return ('+' + p);
 	}
 	else if (n < 0)
 	{
-		b = '-' + p;
-		return (b);
+		return ('-' + p);
 	}
 	else
 	{
