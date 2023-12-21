52ef281ddab22e5f4d2172fb95abfb420862e9de
alexbarasa <alexbarasa847@gmail.com>
2023-12-22 01:03:02 +0300

    Debugging

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+int abs(int x);
 /**
  * largest_number - returns the largest of 3 numbers
  * @a: first integer
@@ -12,11 +13,11 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > -b && -b > -c)
+	if (abs(a) >= abs(b) && abs(b) >= abs(c))
 	{
 		largest = a;
 	}
-	else if (b > -a && -a > -c)
+	else if (abs(b) >= abs(a) && abs(a) >= abs(c))
 	{
 		largest = b;
 	}
@@ -26,4 +27,16 @@ int largest_number(int a, int b, int c)
 	}
 
 	return (largest);
+}
+/**
+ * abs - Returns the absolute value of a number
+ * @x: Integer
+ * Return: Absolute value of x
+ */
+int abs(int x)
+{
+	if (x < 0)
+		return (-x);
+	else
+		return (x);
 }
