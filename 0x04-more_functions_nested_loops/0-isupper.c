c28931e6060fb96e083142c7a5d61bc66359adc2
alexbarasa <alexbarasa847@gmail.com>
2023-12-22 01:22:52 +0300

    Debugging

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+/**
+ * _isupper - Checks the uppercase alphabets
+ * @c: Integer
+ * Return: 1 or 0
+ */
+int _isupper(int c)
+{
+	int i;
+	int j = 'Z';
+
+	for (i = 'A'; i <= j; i++)
+	{
+		if (c == i)
+			return (1);
+		else
+			return (0);
+	}
+}
