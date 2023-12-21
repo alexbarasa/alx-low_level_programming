ab15426d1c191c51cb10443e33eb88e6f469953b
alexbarasa <alexbarasa847@gmail.com>
2023-12-22 01:49:32 +0300

    More functions and nested loops

--- 0x04-more_functions_nested_loops/main.c
+++ 0x04-more_functions_nested_loops/main.c
@@ -1,11 +1,18 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
- * main - Entry point
+ * main - check the code.
  *
  * Return: Always 0.
  */
-int main()
+int main(void)
 {
-	print_alphabet();
-	return (0);
+    char c;
+
+    c = 'A';
+    printf("%c: %d\n", c, _isupper(c));
+    c = 'a';
+    printf("%c: %d\n", c, _isupper(c));
+    return (0);
 }
