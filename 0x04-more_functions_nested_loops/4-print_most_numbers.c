e00d899c962c6fb30f6caed5d2ad1e10dc47df16
alexbarasa <alexbarasa847@gmail.com>
2023-12-22 03:18:32 +0300

    More functions and nested loops

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * print_numbers - Prints numbers 0 to 9
+ * print_most_numbers - Prints numbers 0 to 9 except 2 and 4
  *
  * Return: 0
  */
 void print_most_numbers(void)
+
 {
 	int i;
 	int j = '9';
