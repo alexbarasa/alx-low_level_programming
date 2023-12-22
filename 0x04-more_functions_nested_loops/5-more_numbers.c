d1d2c106785ec815bd03a7b76a65e8b33d632a30
alexbarasa <alexbarasa847@gmail.com>
2023-12-22 03:59:53 +0300

    More functions and nested loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,14 +7,22 @@
 void more_numbers(void)
 {
 	int i, k;
-	int j = '14';
+	int j = 14;
 
 	k = 0;
 	while (k < 10)
 	{
-		for (i = '0'; i <= j; i++)
+		for (i = 0; i <= j; i++)
 		{
-			putchar(i);
+			if (i < 10)
+			{
+				putchar(i + '0');
+			}
+			else
+			{
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
+			}
 		}
 		putchar('\n');
 		k++;
