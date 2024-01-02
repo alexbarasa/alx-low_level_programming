1a0c01969ea34f097cddd3bb9b6f89dca9c16a5c
Alex Barasa <alexbarasa847@gmail.com>
2024-01-02 07:06:59 -0800

    Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * _strlen - Calculates the length of a string
+ *
+ * @s: Character pointer
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	int length;
+
+	length = 0;
+	while (*s != '\0')
+	{
+		putchar(*s);
+		length++;
+		*s++
+	}
+	putchar('\n');
+	return (length);
