fb7772cf0f42319845789f2ce2e9c05de43dad69
alexbarasa <alexbarasa847@gmail.com>
2024-01-02 20:05:50 +0300

    Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_rev - Prints a reversed string
+ *
+ * @s: Character pointer
+ * Return: None
+ */
+void print_rev(char *s)
+{
+	int i, j;
+	char *begin_ptr, *end_ptr, tmp;
+
+	j = _strlen(s);
+	begin_ptr = s;
+	end_ptr = s;
+	for (i = 0; i < j - 1; i++)
+		end_ptr++;
+	for (i = 0; i < j / 2; i++)
+	{
+		tmp = *end_ptr;
+		*end_ptr = *begin_ptr;
+		*begin_ptr = tmp;
+		begin_ptr++;
+		end_ptr--
+	}
+}
