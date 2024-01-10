0fa441efb5b645d52861a75eacfe06f4a1f5422c
alexbarasa <alexbarasa847@gmail.com>
2024-01-10 17:00:01 +0300

    Recursion in C

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * _print_rev_recursion - Prints a string in reverse
+ * @s: Pointer to a string
+ * Return: None
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	_print_rev_recursion(s + 1);
+	putchar(*s);
+}
