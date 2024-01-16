e75003e161ccd0394a5f4366172f2ecb99288d33
alexbarasa <alexbarasa847@gmail.com>
2024-01-16 18:54:42 +0300

    Dynamic memory allocation

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_grid - Frees the memory
+ *
+ * @grid: pointer to a pointer
+ * @height: Integer
+ * Return: None
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+	if (grid == NULL || height <= 0)
+		return;
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
