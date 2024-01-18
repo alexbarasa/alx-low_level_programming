9c0c0d402223283abd9d7c6d17d107641d6322fd
alexbarasa <alexbarasa847@gmail.com>
2024-01-18 13:07:09 +0300

    More malloc and free

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -22,7 +22,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	len2 = _strlen(s2);
 	if (n >= len2)
 		n = len2;
-	result = malloc((len1 + n +1) * sizeof(char));
+	result = malloc((len1 + n + 1) * sizeof(char));
 	if (result == NULL)
 		return (NULL);
 	for (i = 0; i < len1; i++)
