444a2c33a4c29a79f27863ee2dab800e45e3797a
alexbarasa <alexbarasa847@gmail.com>
2024-01-22 23:03:10 +0300

    Structure and typedef

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -5,9 +5,8 @@
  * free_dog - Clears the memory occupied by dog_t
  *
  * @d: Pointer to dog_t structure
- * @Return: None
  */
- void free_dog(dog_t *d)
+void free_dog(dog_t *d)
 {
 	if (d != NULL)
 	{
