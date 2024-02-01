8cb5eaa79cf96ebde69275903a12bc7867839846
alexbarasa <alexbarasa847@gmail.com>
2024-02-01 20:34:14 +0300

    Singly linked lists

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,7 @@
 section .data
     hello_msg db "Hello, Holberton", 10, 0  ; Message to be printed, followed by newline and null terminator
-
 section .text
     extern printf  ; Declare printf as an external symbol
-
 global _start
 
 _start:
@@ -11,10 +9,7 @@ _start:
     mov rdi, hello_msg  ; Load address of the message string into rdi
     xor rax, rax       ; Clear rax to indicate no floating-point arguments
     call printf        ; Call printf function
-
     ; Exit the program
     mov rax, 60        ; System call number for exit
     xor rdi, rdi       ; Clear rdi (exit code 0)
     syscall            ; Call kernel
-
-
