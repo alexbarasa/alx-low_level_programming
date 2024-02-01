769b33f0b208cc4f1b49689f609baf1e1aeb1ce8
alexbarasa <alexbarasa847@gmail.com>
2024-02-01 15:29:47 +0300

    Singly linked lists

---
+++ 0x12-singly_linked_lists/2-add_node_end.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+#include <string.h>
+/**
+ * add_node_end - Prints the added node
+ *
+ * @head: Starting node
+ * @str: String pointer
+ * Return: The address of the new element/NULL
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+
+	if (str == NULL)
+		return (NULL);
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+}
