c0a2017b98f62f7cb9b6eb87e5c26b0b82c516ac
alexbarasa <alexbarasa847@gmail.com>
2024-02-01 20:23:40 +0300

    Singly linked lists

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 /**
- * __attribute__((constructor)) before_main - Prints text before main is executed
+ * __attribute__((constructor)) before_main - Prints text before
+ * main is executed
  *
  */
 void __attribute__((constructor)) before_main(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
 
