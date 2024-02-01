3be41e5dc880aa3dbe6799a5852375bbc0a63918
alexbarasa <alexbarasa847@gmail.com>
2024-02-01 20:17:48 +0300

    Singly linked lists

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * __attribute__((constructor)) before_main - Prints text before main is executed
+ *
+ */
+void __attribute__((constructor)) before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
+
