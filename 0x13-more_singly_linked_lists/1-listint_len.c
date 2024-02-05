1853d292d151dba08fbdff130b4128bde7a3a789
alexbarasa <alexbarasa847@gmail.com>
2024-02-05 22:20:26 +0300

    More singly linked lists

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * listint_len - Prints how many elements in list_t list
+ *
+ * @h: Pointer to the nodes memory
+ * Return: Number of nodes in the list
+ */
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *current = h;
+	size_t len = 0;
+
+	while (current != NULL)
+	{
+		len++;
+		current = current->next;
+	}
+	return (len);
+}
