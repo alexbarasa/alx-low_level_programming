8bb5337bd86a92e79a062ee7f88991280595cfaf
Alex Barasa <alexbarasa847@gmail.com>
2024-02-06 06:19:31 -0800

    More singly linked lists

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_listint - Frees the list_t list
+ *
+ * @head: Pointer to the head of the list
+ */
+void free_listint(list_t *head)
+{
+	list_t *current, *temp;
+
+	current = head;
+	while (current != NULL)
+	{
+		temp = current;
+		current = current->next;
+		free(temp->n);
+		free(temp);
+	}
+}
