2c1a0b8173d5b2d20072b64772ae71069d6b67dd
alexbarasa <alexbarasa847@gmail.com>
2024-02-12 14:21:50 +0300

    File input output

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+/**
+ * create_file - Creates a file with the specified content
+ * @filename: Name of the file to create
+ * @text_content: Content to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, write_status;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		write_status = write(fd, text_content, strlen(text_content));
+		if (write_status == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
+}
+
