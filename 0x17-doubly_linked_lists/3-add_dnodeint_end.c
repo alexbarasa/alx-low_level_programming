5c2f239e239cc14e49de98673d719814acb256fe
Alex Barasa <alexbarasa847@gmail.com>
2024-03-21 14:25:50 -0700

    Doubly linked lists

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -11,6 +11,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
 	dlistint_t *last = *head;
+
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
@@ -22,10 +23,8 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		return (new_node);
 	}
 	while (last->next != NULL)
-	{
 		last = last->next;
-		last->next = new_node;
-		new_node->prev = last;
-		return (new_node);
-	}
+	last->next = new_node;
+	new_node->prev = last;
+	return (new_node);
 }
